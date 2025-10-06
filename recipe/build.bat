34080ba3a408e6e2ada7bb37e551b096e0a6f879
Stéphane Gouache <stephane.gouache@orange.com>
2025-10-06 15:45:38 +0200

    Adjust installation path

--- recipe/build.bat
+++ recipe/build.bat
@@ -8,4 +8,4 @@ REM Build
 cmake --build builds\conda --parallel --target khiopsdriver_file_gcs
 
 REM Copy the lib for the driver package
-cmake --install builds\conda --prefix %PREFIX%
+cmake --install builds\conda --prefix %PREFIX%\Library
